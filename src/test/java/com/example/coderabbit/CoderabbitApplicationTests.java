bb71515095096b5979b9c24b69b78cde78de9ef5
Oh-Myeongjae <omjcws@gmail.com>
2025-09-27 05:03:15 +0900

    커밋

---
+++ src/test/java/com/example/coderabbit/CoderabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coderabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderabbitApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
